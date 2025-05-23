2cdf3e2416c4f23ee6500fe69fc690545a48675e
Fajar Syahputra <fasya080895@gmail.com>
2025-05-24 05:09:21 +0800

    register masyarakat

--- db/mongodb/connection.js
+++ db/mongodb/connection.js
@@ -1,108 +1,23 @@
-// const { MongoClient, name } = require('mongodb'); 
-
-// // Ambil URI dari .env
-// const uri = process.env.URI_DB_MONGO;
-// const dbName = process.env.NAMA_DB_MONGO;
-
-// // const uri = 'mongodb://diskominfosandi:Kominfo2018@localhost:27017';
-// // const dbName = "warga_bicara"
- 
-
-// if (!uri) {
-//   console.error("❌ URI MongoDB tidak ditemukan di .env (MONGO_URI)");
-//   process.exit(1);
-// }
-
-// const client = new MongoClient(uri);
-
-// // const database = client.db("warga_bicara");
-// const db = client.db(dbName);
-// console.log(db); 
-// const a = database.getCollectionNames("users");
-
-// buatCollections("users");
-
-//  function buatCollections(namaCollections) {
-//     // var collection = db.collection(namaCollections);
-//     // collection.create().then(
-//     //     () => console.log('Collections "'+namaCollections+'" berhasil dibuat'),
-//     //     err => console.error('Collections "'+namaCollections+'" gagal dibuat atau sdh dibuat sebelumnya')
-//     // );
-
-//     var collection =  database.getCollectionNames(namaCollections);
-//     if (collection===true) {
-//         console.log("Sudah ada");
-//     }else{
-//         console.log("Belum ada buat baru");
-//     }
-
-
-// //     db.createCollection(
-// //   "<viewName>",
-// //   {
-// //     "viewOn" : "<source>",
-// //     "pipeline" : [<pipeline>],
-// //     "collation" : { <collation> }
-// //   }
-// // )
-// }
-
-
-// module.exports = db;
-
-
-
-// ===================================================================
-
-// const { MongoClient } = require("mongodb");
-// // Replace the uri string with your connection string
-// // const uri = "<connection string uri>";
-// // // Ambil URI dari .env
-// const uri = "mongodb://diskominfosandi:Kominfo2018@localhost:27017/?authMechanism=SCRAM-SHA-1";
-// // const uri = process.env.URI_DB_MONGO;
-// const dbName = process.env.NAMA_DB_MONGO;
-// const client = new MongoClient(uri);
-// const db = client.connect(dbName)
-
-// const db = dba.then((result) => 
-//     console.log("db connect"))
-// .catch((err)=> console.log(err))
-
-// async function run() {
-//   try {
-//     const database = client.db(dbName);
-//     const movies = database.collection('users');
-//     // Queries for a movie that has a title value of 'Back to the Future'
-//     const query = { nama: 'Riswan' };
-//     const movie = await movies.find(query);
-//     console.log(movie);
-//     console.log("queryqueryqueryquery");
-//   } finally {
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// module.exports = db;
-
-
-
-// ===================================
-
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-// const uri = process.env.URI_DB_MONGO;
-// const dbName = process.env.NAMA_DB_MONGO;
+const dbUsername = process.env.USERNAME_DB_MONGO
+const dbPassword = process.env.PASSWORD_DB_MONGO
+const dbHost = process.env.HOST_DB_MONGO
 
-const uri = "mongodb://diskominfosandi:Kominfo2018@localhost:27017/?authMechanism=SCRAM-SHA-1";
-// const uri = process.env.URI_DB_MONGO;
+// const uri = `mongodb://`+dbUsername+`:`+dbPassword+`@localhost:27017/?authMechanism=SCRAM-SHA-1`;
+// const uri = `mongodb+srv://diskominfosandi:Kominfo2018@cluster0.9zdgtpl.mongodb.net/`; 
+const uri = `mongodb+srv://`+dbUsername+`:`+dbPassword+`@`+dbHost+``;
+// const uri = `mongodb://`+dbUsername+`:`+dbPassword+`@`+dbHost+``; 
+ 
 const dbName = process.env.NAMA_DB_MONGO;
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+const client = new MongoClient(uri);
 
 let db;
 
--- index.js
+++ index.js
@@ -2,14 +2,42 @@ require('dotenv').config();
 const express = require('express')
 const volleyball = require('volleyball');
 const cors = require('cors');
+var path = require("path");
+
 
 const app = express()
 const port = 3000
+app.use(express.json()); // ⬅️ Ini wajib
 
 app.use(volleyball);
-app.use(cors({ origin : '*' }));
+
+// app.use(cors({ origin : '*' })); 
+
+const allowedOrigins = [
+//   'https://domain1.com',
+//   'https://domain2.com',
+//   'http://localhost:3000',
+//   '*',
+  'http://localhost:3000',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Jika kamu pakai cookie/token
+}));
+
 
 const {connectMongo} = require('./db/mongodb/connection')
+
+
 
 
 app.get('/', async (req, res) => { 
@@ -17,12 +45,32 @@ app.get('/', async (req, res) => {
     const users = db.collection('users'); //memilih collection yang mau di query
     const data = await users.find().toArray(); //query data
     console.log('📦 Data pengguna:', data);
-    res.send(data) 
-
+    res.send(data)  
 })
 
+const reg_masyarakat = require('./api/auth/registration_masyarakat');
+app.use('/api/v1/reg_masyarakat', reg_masyarakat);
 
 
+
+
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found data - ' + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => { console.log(`Example app listening on port ${port}`) })
